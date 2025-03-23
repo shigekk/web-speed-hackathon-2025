90fac1f3c5bf5d5bcb139db24262aaca6f8658ea
shigek <mizokyuu114@gmail.com>
2025-03-23 16:47:24 +0900

    sr2

--- workspaces/client/src/pages/not_found/components/NotFoundPage.tsx
+++ workspaces/client/src/pages/not_found/components/NotFoundPage.tsx
@@ -1,12 +1,6 @@
-import { lazy, Suspense } from 'react';
 import { createStore } from '@wsh-2025/client/src/app/createStore';
+import { RecommendedSection } from '@wsh-2025/client/src/features/recommended/components/RecommendedSection';
 import { useRecommended } from '@wsh-2025/client/src/features/recommended/hooks/useRecommended';
-
-const RecommendedSection = lazy(() =>
-  import('@wsh-2025/client/src/features/recommended/components/RecommendedSection').then((m) => ({
-    default: m.RecommendedSection,
-  }))
-);
 
 export const prefetch = async (store: ReturnType<typeof createStore>) => {
   const modules = await store
@@ -27,14 +21,9 @@ export const NotFoundPage = () => {
         <section className="mb-[32px] flex w-full flex-col items-center justify-center gap-y-[20px]">
           <h1 className="text-[32px] font-bold text-[#ffffff]">ページが見つかりませんでした</h1>
           <p>あなたが見ようとしたページは、残念ながら見つけられませんでした。</p>
-          <img alt="" loading="lazy" className="h-auto w-[640px]" src="/public/animations/001.gif" />
+          <img alt="" className="h-auto w-[640px]" src="/public/animations/001.gif" />
         </section>
-
-        <section>
-          <Suspense fallback={<div className="text-white">おすすめ読み込み中...</div>}>
-            {module != null ? <RecommendedSection module={module} /> : null}
-          </Suspense>
-        </section>
+        <section>{module != null ? <RecommendedSection module={module} /> : null}</section>
       </div>
     </>
   );
